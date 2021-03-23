d455cf00b25b8a79de5ba77588923af4bcfe8831
Gordiig <gordiig@gmail.com>
2021-03-23 17:55:09 +0300

    Add project

---
+++ Fatty-iOS/Fatty-iOS/Fatty_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fatty_iOSApp.swift
+//  Fatty-iOS
+//
+//  Created by Dmitry Gorin on 23.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Fatty_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
